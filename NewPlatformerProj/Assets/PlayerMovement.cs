100a2cffe36b9d448efc9625e6afbc60447b26e8
Ethan Malenchek <Ethan@Ethans-MacBook-Pro-2.local>
2018-10-08 16:54:30 -0400

    Added all essential assets and builds for project as well as the CharacterController and PlayerMovement scripts

---
+++ NewPlatformerProj/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    float horizontal = 0f;
+
+    public float runSpd = 40f;
+
+    bool jump = false;
+
+	// Update is called once per frame
+	void Update () {
+
+        horizontal = Input.GetAxisRaw("Horizontal") * runSpd; // horizontal movement update
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        // Moves Character
+        controller.Move(horizontal * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
