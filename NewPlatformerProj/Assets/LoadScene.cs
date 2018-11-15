7268e101c522b927b5ffa415ddb87093a2657400
Kyle <kc712414@ohio.edu>
2018-11-15 09:32:59 -0500

    Added LoadScene script and simple Scene selection

---
+++ NewPlatformerProj/Assets/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public Button b1;
+    public string level;
+    // Use this for initialization
+    void Start()
+    {
+            b1.onClick.AddListener(TaskOnClick);
+    }
+    
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(level,LoadSceneMode.Single);
+    }
+
+}
--- NewPlatformerProj/Assets/Player.cs
+++ NewPlatformerProj/Assets/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
@@ -32,6 +33,10 @@ public class Player : MonoBehaviour {
         {
             playerStats.lives -= 1;
             GameMaster.KillPlayer(this);
+        }
+        if (col.gameObject.name == "End")
+        {
+            SceneManager.LoadScene("SceneSelect", LoadSceneMode.Single);
         }
     }
 }
